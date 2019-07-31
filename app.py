f126cdc4222436366595300700e1b8cb1b377c23
Oliver Smith <oli-smith@live.co.uk>
2019-07-31 14:54:12 +0100

    Test: app is working locally but not on Heroku, changing config settings in app.py to see if my env variables have messed up Heroku

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 # config settings (includes DB connection, debugger, and secret_key)
 import os
-app.config.from_object(os.environ['APP_SETTINGS'])
-# sanity check for environemt variable:
-print(os.environ['APP_SETTINGS'])
+# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(config.DevelopmentConfig)
+
 
 # create the sqlalchemy object
 db = SQLAlchemy(app)
