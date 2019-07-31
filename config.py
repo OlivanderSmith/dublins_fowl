602017220232af131a754269bdaa8e88619d5208
Oliver Smith <oli-smith@live.co.uk>
2019-07-31 14:08:34 +0100

    updated secret key to more secure randomly selected and longer key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # default config parent class
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = "\x96\xdd\K\xd4Y\x8f\xba\xa3\xc8\xce>E\tJ?w\x15\xf2\xeaS\xa1xdc"
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
